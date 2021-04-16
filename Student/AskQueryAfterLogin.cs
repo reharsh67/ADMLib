278213c3f821f4ffc6bd544ed560bf455abba173
Reharsh <reharshdeshpande@gmail.com>
2021-04-16 17:33:33 +0530

    Dump Query Response and Query Posting Logic

---
+++ Student/AskQueryAfterLogin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace MyTestLib
+{
+    public class AskQueryAfterLogin
+    {
+
+        public int Ask_Que_Again(EnqueryFields ef)
+        {
+
+            const string strcon = "Server=localhost\\SQLEXPRESS;Database=onlineadmission;Trusted_Connection=True;";
+            String query2 = "insert into adm_tbl_student_queries (r_appid,r_query,r_time_posted) values (@r_uid,@r_query,@r_time_posted)";
+
+            DateTimeOffset now = (DateTimeOffset)DateTime.UtcNow;
+           SqlConnection con = new SqlConnection(strcon);
+           SqlCommand cmd = new SqlCommand(query2, con);
+            cmd.CommandType = CommandType.Text;
+            try
+            {
+                
+                SqlCommand cmd2 = new SqlCommand(query2, con);
+                cmd2.Connection = con;
+                con.Open();
+                cmd2.Parameters.AddWithValue("@r_query", ef.Query);
+                cmd2.Parameters.AddWithValue("@r_time_posted", now.ToString());
+                cmd2.Parameters.AddWithValue("@r_uid", ef.AppID);
+                cmd2.ExecuteNonQuery();
+                
+                con.Close();
+                
+                return 0;
+
+
+            }
+            catch (Exception ex)
+            {
+                return 1;
+                throw;
+            }
+            finally
+            {
+                con.Close();
+                
+
+            }
+        }
+    }
+}
---
+++ Student/ViewQueryRespLogic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace MyTestLib
+{
+    public class ViewQueryRespLogic
+    {
+        public DataTable View_Response(EnqueryFields ef)
+        {
+
+
+            const string strcon = "Server=localhost\\SQLEXPRESS;Database=onlineadmission;Trusted_Connection=True;";
+            String query = "select * from adm_tbl_student_queries where r_appid=@r_appid ";
+            SqlConnection con = new SqlConnection(strcon);
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.CommandType = CommandType.Text;
+            DataTable dt = new DataTable { TableName = "MyTable" };
+            cmd.Parameters.AddWithValue("@r_appid", ef.AppID);
+            cmd.Connection = con;
+            try
+            {
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                dt.Load(dr);
+                
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+                cmd.Dispose();
+
+            }
+            return dt;
+        }
+    }
+}
